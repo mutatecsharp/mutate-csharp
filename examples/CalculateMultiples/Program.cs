8e84b8f6b29a180ea5e525f5536ac98ec5a15185
Khemi <jiajinchew@gmail.com>
2024-04-03 17:44:52 +0100

    chore: add examples

---
+++ examples/CalculateMultiples/Program.cs
@@ -0,0 +1,14 @@
+﻿Console.WriteLine(SumOfMultiplesOf3And5());
+return;
+
+static long SumOfMultiplesOf3And5()
+{
+  long sum = 0;
+  
+  for (var i = 0; i < 1000; i++)
+  {
+    if (i % 3 == 0 || i % 5 == 0) sum += i;
+  }
+
+  return sum;
+}
